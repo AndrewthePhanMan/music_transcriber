e06c5f8c4c103d8eaf754c60f77d718db4200e8f
thePhanMan15 <andrewphan2007@gmail.com>
2026-01-06 21:56:06 -0500

    Added note.py

---
+++ note.py
@@ -0,0 +1,15 @@
+class Note:
+    def __init__(self, onset, duration, pitch, frequency):
+        """
+            Initializes an instance of the Note class.
+
+            Parameters:
+            - onset:
+            - duration:
+            - pitch:
+            - frequency:
+        """
+        self.onset = onset
+        self.duration = duration
+        self.pitch = pitch
+        self.frequency = frequency
--- pitch_detection.py
+++ pitch_detection.py
@@ -7,9 +7,13 @@ def find_pitch(audio, sample_rate, onset):
     """
     # Creates a 100 millisecond slice of the audio, beginning from the onset
     start = int(onset * sample_rate)
-    end = int((onset + 0.1) * sample_rate) # 100 milliseconds ahead of "start"
+    end = int((onset + 0.1) * sample_rate) # 100 ms ahead of "start"
     end = min(end, len(audio)) # Checks if "end" goes beyond the audio's length (last note)
     audio_slice = audio[start:end]
+
+    # Checks if the audio_slice is long enough to be analyzed (20 ms)
+    if len(audio_slice) < int(0.02 * sample_rate):
+        return None
 
     # 
     pitches = librosa.yin(y=audio_slice, fmin=librosa.note_to_hz("C2"), fmax=librosa.note_to_hz("C7"), sr=sample_rate)
